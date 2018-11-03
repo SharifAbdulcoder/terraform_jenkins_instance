914fe646f3316ec72843779376e8918952f59be8
SharifAbdulcoder <43179042+SharifAbdulcoder@users.noreply.github.com>
2018-11-03 13:58:29 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "newname" {
   ami                       = "${lookup(var.amis, var.region)}"
   instance_type             = "${var.instancetype}"
   key_name                  = "terraformjenkins"
-  associate_with_private_ip = "true"
+  associate_public_ip_address = "true"
   count                     = "${var.count}"
 }
